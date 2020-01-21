15e0025d839be6cd52192ffd6fa7f869a9ace68a
Ofoe Fiergbor <ofoefiergbor16@gmail.com>
2020-01-21 21:56:03 +0000

    linked an api call

--- src/Components/MemePage.jsx
+++ src/Components/MemePage.jsx
@@ -1,31 +1,48 @@
 import React, { Component } from 'react'
 import randomMeme from '../img/IMG_20200116_130709.jpg'
 
+const memeAPI = '"https://api.imgflip.com/get_memes"'
+
 class MemePage extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            randomImg: randomMeme,
+            defaultImg: randomMeme,
             topText: '',
-            bottomText: ''
+            bottomText: '',
+            allMemeImgs: []
         }
     }
-    handleTopForm=(e)=>{
+    componentDidMount() {
+        fetch(memeAPI)
+          .then(res => res.json())
+          .then(res => {
+        const { memes } = res.data
+        this.setState({ allMemeImgs: memes })
+        })
+      }
+    handleTopForm = (e) => {
         this.setState({
-            topText:e.target.value
+            topText: e.target.value
         })
     }
 
-    handleBottomText=e=>{
+    handleBottomText = e => {
         this.setState({
-            bottomText:e.target.value   
+            bottomText: e.target.value
         })
     }
-    handleSubmit=()=>{
+    handleSubmit = () => {
         alert('it works')
     }
 
+    handleSubmit = e => {
+        e.preventDefault()
+        const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length)
+        const randMemeImg = this.state.allMemeImgs[randNum].url
+        this.setState({ defaultImg: randMemeImg })
+    }
     render() {
         const { topText, bottomText } = this.state
         return (
@@ -35,17 +52,17 @@ class MemePage extends Component {
                     <div className="col-md-7">
                         <div className='meme'>
                             <h2 className='top'>{topText}</h2>
-                            <img src={this.state.randomImg} alt='random meme' />
+                            <img src={this.state.defaultImg} alt='random meme' />
                             <h2 className='bottom'>{bottomText}</h2>
                         </div>
                     </div>
                     <div className="col-md-5">
                         <form onSubmit={this.handleSubmit}>
                             <div className="form-group">
-                                <input type='text' className="form-control" placeholder='Top Text' value={topText} onChange={this.handleTopForm}/>
+                                <input type='text' className="form-control" placeholder='Top Text' value={topText} onChange={this.handleTopForm} />
                             </div>
                             <div className="form-group">
-                                <input type='text' className="form-control" placeholder='Bottom Text' value={bottomText} onChange={this.handleBottomText}/>
+                                <input type='text' className="form-control" placeholder='Bottom Text' value={bottomText} onChange={this.handleBottomText} />
                             </div>
 
                             <br />
